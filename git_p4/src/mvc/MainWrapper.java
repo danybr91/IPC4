721c4256b6e83687c51470651738f80a734d6ed1
Daniel Belén Del Río <dbelendelrio@gmail.com>
2015-12-22 00:47:54 +0100

    Signed-off-by: Daniel Belén Del Río <dbelendelrio@gmail.com>

--- git_p4/src/mvc/MainWrapper.java
+++ git_p4/src/mvc/MainWrapper.java
@@ -1,25 +1,27 @@
 package mvc;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+
 import javax.swing.Action;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.SwingConstants;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 /**
  * Clase que implementa la interfaz grafica de la aplicacion
@@ -59,11 +61,14 @@ public class MainWrapper extends JFrame implements PropertyChangeListener {
 				try {
 					Partido miModelo = new Partido();
 					MainWrapper vista = new MainWrapper(miModelo);
+					vista.setTitle("Vista 1");
 					vista.setGuardarAction(new GuardarAction(miModelo, vista));
 					vista.setVisible(true);
 					// Segunda ventana
 					MainWrapper vista2 = new MainWrapper(miModelo);
 					vista2.setGuardarAction(new GuardarAction(miModelo, vista2));
+					vista2.setTitle("Vista 2");
+					vista2.setLocation(vista.getX()+300, vista.getY());
 					vista2.setVisible(true);
 
 				} catch (Exception e) {
@@ -77,95 +82,109 @@ public class MainWrapper extends JFrame implements PropertyChangeListener {
 	 * Create the frame.
 	 */
 	public MainWrapper(Partido p) {
-		// Anhadimos listener
+		setResizable(false);
+		// Añadimos listener
 		p.addPropertyChangeListener(this);
-
+		
+		// Creamos la ventana principal
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 555, 339);
+		setBounds(100, 100, 280, 210);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 
+		// Contenedor del formulario
 		panel = new JPanel();
+		// Borde con título pero sin línea, pues es el único contenido de la
+		// ventana. Incorpora un pequeño padding por cuestiones visuales
+		panel.setBorder(new TitledBorder(new EmptyBorder(20, 20, 20, 20), 
+				"Introduce los datos del partido:", TitledBorder.CENTER, 
+				TitledBorder.TOP, null, new Color(0, 0, 0)));
 		contentPane.add(panel, BorderLayout.CENTER);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] { 30, 86, 0, 0, 45 };
-		gbl_panel.rowHeights = new int[] { 23, 0, 0, 0, 0, 0, 0 };
-		gbl_panel.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0 };
-		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel.rowHeights = new int[] {30, 0, 0, 0, 30, 0};
+		gbl_panel.columnWidths = new int[] {0, 0};
+		gbl_panel.columnWeights = new double[] { 0.0, 1.0 };
+		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0 };
 		panel.setLayout(gbl_panel);
 		panel.setMaximumSize(getMinimumSize());
 
+		// Etiqueta local y posición en GridLayout
 		JLabel lblLocal = new JLabel("Local:");
 		GridBagConstraints gbc_lblLocal = new GridBagConstraints();
 		gbc_lblLocal.anchor = GridBagConstraints.EAST;
 		gbc_lblLocal.insets = new Insets(0, 0, 5, 5);
-		gbc_lblLocal.gridx = 1;
-		gbc_lblLocal.gridy = 2;
+		gbc_lblLocal.gridx = 0;
+		gbc_lblLocal.gridy = 1;
 		panel.add(lblLocal, gbc_lblLocal);
 
+		// Cammpo de texto local y posición en GridLayout
 		textLocal = new JTextField();
 		GridBagConstraints gbc_textLocal = new GridBagConstraints();
-		gbc_textLocal.anchor = GridBagConstraints.WEST;
-		gbc_textLocal.insets = new Insets(0, 0, 5, 5);
-		gbc_textLocal.gridx = 2;
-		gbc_textLocal.gridy = 2;
+		gbc_textLocal.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textLocal.insets = new Insets(0, 0, 5, 0);
+		gbc_textLocal.gridx = 1;
+		gbc_textLocal.gridy = 1;
 		panel.add(textLocal, gbc_textLocal);
 		textLocal.setColumns(10);
 
+		// Etiqueta visitante y posición en GridLayout
 		JLabel lblVisitante = new JLabel("Visitante:");
 		GridBagConstraints gbc_lblVisitante = new GridBagConstraints();
 		gbc_lblVisitante.anchor = GridBagConstraints.EAST;
 		gbc_lblVisitante.insets = new Insets(0, 0, 5, 5);
-		gbc_lblVisitante.gridx = 1;
-		gbc_lblVisitante.gridy = 3;
+		gbc_lblVisitante.gridx = 0;
+		gbc_lblVisitante.gridy = 2;
 		panel.add(lblVisitante, gbc_lblVisitante);
 
+		// Cammpo de texto visitante y posición en GridLayout
 		textVisitante = new JTextField();
 		GridBagConstraints gbc_textVisitante = new GridBagConstraints();
-		gbc_textVisitante.anchor = GridBagConstraints.WEST;
-		gbc_textVisitante.insets = new Insets(0, 0, 5, 5);
-		gbc_textVisitante.gridx = 2;
-		gbc_textVisitante.gridy = 3;
+		gbc_textVisitante.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textVisitante.insets = new Insets(0, 0, 5, 0);
+		gbc_textVisitante.gridx = 1;
+		gbc_textVisitante.gridy = 2;
 		panel.add(textVisitante, gbc_textVisitante);
 		textVisitante.setColumns(10);
 
+		// Etiqueta Fecha y posición en GridLayout
 		JLabel lblFecha = new JLabel("Fecha:");
 		GridBagConstraints gbc_lblFecha = new GridBagConstraints();
 		gbc_lblFecha.anchor = GridBagConstraints.EAST;
 		gbc_lblFecha.insets = new Insets(0, 0, 5, 5);
-		gbc_lblFecha.gridx = 1;
-		gbc_lblFecha.gridy = 4;
+		gbc_lblFecha.gridx = 0;
+		gbc_lblFecha.gridy = 3;
 		panel.add(lblFecha, gbc_lblFecha);
 
+		// Cammpo de texto Fecha y posición en GridLayout
 		textFecha = new JTextField();
 		GridBagConstraints gbc_textFecha = new GridBagConstraints();
-		gbc_textFecha.anchor = GridBagConstraints.WEST;
-		gbc_textFecha.insets = new Insets(0, 0, 5, 5);
-		gbc_textFecha.gridx = 2;
-		gbc_textFecha.gridy = 4;
+		gbc_textFecha.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFecha.insets = new Insets(0, 0, 5, 0);
+		gbc_textFecha.gridx = 1;
+		gbc_textFecha.gridy = 3;
 		panel.add(textFecha, gbc_textFecha);
 		textFecha.setColumns(10);
-
+		GridBagConstraints gbc_btnGuardar_1 = new GridBagConstraints();
+		gbc_btnGuardar_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnGuardar_1.insets = new Insets(0, 0, 5, 0);
+		gbc_btnGuardar_1.gridx = 1;
+		gbc_btnGuardar_1.gridy = 4;
+		
+		// Botón que guarda los cambios
 		btnGuardar_1 = new JButton("Guardar");
-		btnGuardar_1.setHorizontalAlignment(SwingConstants.LEFT);
 		btnGuardar_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
 			}
 		});
-		GridBagConstraints gbc_btnGuardar_1 = new GridBagConstraints();
-		gbc_btnGuardar_1.insets = new Insets(0, 0, 0, 5);
-		gbc_btnGuardar_1.gridx = 2;
-		gbc_btnGuardar_1.gridy = 5;
 		panel.add(btnGuardar_1, gbc_btnGuardar_1);
-
-		JLabel lblNewLabel = new JLabel("Introduce los datos del partido");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(lblNewLabel, BorderLayout.NORTH);
 	}
 
+	/*
+	 * ACCION GUARDAR
+	 */
 	public void setGuardarAction(Action action) {
 		btnGuardar_1.setAction(action);
 	}
